5c3c40f0a86260769730ea41fa915a3a33ef5a5e
Ashish Shinde <ashish.shinde@lemmatechnologies.com>
2024-06-24 16:24:12 +0530

    add logo in qr code

--- main.go
+++ main.go
@@ -9,7 +9,9 @@ import (
 	// "log"
 	// "os"
 
-	qrcode "github.com/yeqown/go-qrcode"
+	// qrcode "github.com/yeqown/go-qrcode"
+	"github.com/yeqown/go-qrcode/v2"
+	"github.com/yeqown/go-qrcode/writer/standard"
 )
 
 // func WithLogoImageFilePNG(f string) ImageOption {
@@ -22,9 +24,40 @@ func main() {
 		fmt.Printf("could not generate QRCode: %v", err)
 	}
 
-	// save file
-	if err := qrc.Save("./repo-qrcode.jpeg"); err != nil {
-		fmt.Printf("could not save image: %v", err)
+	// // save file
+	// if err := qrc.Save("./repo-qrcode.jpeg"); err != nil {
+	// 	fmt.Printf("could not save image: %v", err)
+	// }
+
+	options := []standard.ImageOption{
+		// standard.WithHalftone("./a-img (1).png"),
+		// standard.WithQRWidth(250),
+		standard.WithQRWidth(10),
+
+		standard.WithLogoImageFilePNG("./a-img.png"), // here the size of image is - width-90pixels,Height-80pixels
+		// standard.WithLogoImage()
 	}
+	filename := "./qr_code.png"
+
+	// if *transparent {
+	// 	options = append(
+	// 		options,
+	// 		standard.WithBuiltinImageEncoder(standard.PNG_FORMAT),
+	// 		standard.WithBgTransparent(),
+	// 	)
+	// 	filename = "./halftone-qr-transparent.png"
+	// }
+
+	w0, err := standard.New(filename, options...)
+	if err != nil {
+		fmt.Printf("error %v:", err)
+	}
+	// handleErr(err)
+
+	err = qrc.Save(w0)
+	if err != nil {
+		fmt.Printf("error %v:", err)
+	}
+	// handleErr(err)
 
 }
--- readme.txt
+++ readme.txt
@@ -14,3 +14,10 @@ For generating the QR code, packages in golang inclued, skip2/go-qr, yeqown/go-q
                     last updated 2 years ago, 
                     has many other formats like, 2 of 5,Aztec Code,Codabar,Code 128,
                     Code 39,Code 93,Datamatrix,EAN 13,EAN 8,PDF 417, and QR code
+
+
+so here we are using go-qrcode.v2 which is updagrade to go-qrcode
+
+go-qrcode.v2 is a major upgrade from v1, and it is not backward compatible. 
+v2 redesigned the API, and it is more flexible and powerful. 
+Features are split into different modules (according to functionality).
